e3f9c865efbbb476d2e5aad0375f6fa1728d4e00
laviprog <areyouready@internet.ru>
2024-10-16 22:32:21 +0300

    initial commit

---
+++ src/main/java/com/laviprog/pastes/PastesApplication.java
@@ -0,0 +1,13 @@
+package com.laviprog.pastes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laviprog/pastes/PastesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laviprog.pastes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
