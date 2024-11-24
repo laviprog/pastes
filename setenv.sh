2733e4aa6753a58b38c018734be0559ca6c2971e
laviprog <areyouready@internet.ru>
2024-11-24 18:04:29 +0300

    environments and docker

---
+++ setenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export TOKEN_SINGING_KEY=
+export USERNAME_DB=
+export PASSWORD_DB=
+export NAME_DB=
+export PORT_DB=
+export SERVICE_ENDPOINT=
+export ACCESS_KEY_ID=
+export SECRET_ACCESS_KEY=
+export REGION=
+export BUCKET_DEFAULT=
