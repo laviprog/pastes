dceda83ff65aebd9f195827168d2cecf594e5f0a
laviprog <areyouready@internet.ru>
2024-12-01 18:15:30 +0300

    removed unused method

--- src/main/java/com/laviprog/pastes/repositories/UserRepository.java
+++ src/main/java/com/laviprog/pastes/repositories/UserRepository.java
@@ -11,8 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-    Optional<User> findByEmail(String email);
-
     Optional<User> findByUsernameOrEmail(String username, String email);
 
     @Query(value = "SELECT * FROM users WHERE user_id != :id AND (username = :username OR email = :email) LIMIT 1", nativeQuery = true)
